138c9132e5749897c484156bfed2825b448072e1
Nicolas <nicolas.fouchard94370@gmail.com>
2020-02-09 13:18:58 +0100

    Create SQL files for the example

---
+++ C/Merge Fusion/example3.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Customer (
+  customerId varchar(255),
+  lastName varchar(255),
+  firstName varchar(255),
+  email varchar(255),
+  phoneNumber varchar(255),
+  address varchar(255),
+  town varchar(255),
+  password varchar(255),
+  enable tinyint
+);
+
+INSERT INTO Customer ("aaafdsqfsdqfsqdfsqf","Paul","Jean","jenesaispas@gmail.com","0151588496","4 rue des arbres","Sucy","1ds32d13s1f3ddfs1sdf323d",1);
+INSERT INTO Customer ("aaafqsdfsdqfsdqfsdqfqdf","Marc","Jean","lolol@gmail.com","0151588496","8 rue des arbres","Sucy","srthsthshthh",1);
+INSERT INTO Customer ("aaafqsdfdsqfsqdfsdffqd","Pierre","Jean","jp48652@gmail.com","0151588496","6 rue des arbres","Sucy","strhtsrhhtrsstrh",1);
+INSERT INTO Customer ("aaad,qsdfqsdfdqsfdfs","Jean","Jean","jp963963@gmail.com","0151588496","8 rue des arbres","Sucy","srthstrhtsrhtshrtsr",1);
+INSERT INTO Customer ("aaafefefsfdnsfnsqdkjfnljsdf","Paul","Fernand","jppj@gmail.com","0151588496","4 rue des plages","Sucy","srthtsrhtrhsrthtsrhtrs",0);
+INSERT INTO Customer ("aaafesfefsfes","Nicolas","Jean","gmail@gmail.com","0151588496","4 rue des navets","Sucy","sthstrhstrhtrsh",1);
+INSERT INTO Customer ("aaafesfesffseffe","Max","Jean","marabouuuuu@gmail.com","0151588496","4 rue des tortues","Sucy","htsstrhstrhstrhthrs",1);
+INSERT INTO Customer ("aaagggggggggggggggggggghhhhhhhhhhh","Porter","Sam","sp@gmail.com","0151588496","2 rue des arbres","Sucy","hhstrthrtrhhtrshhstr",1);
+INSERT INTO Customer ("aaafrfseffffffffffffffff","Paulo","Jeanne","jp@gmail.com","0151588496","1 rue des arbres","Sucy","htrshsrhthrstrhhtsrhtrht",0);
+INSERT INTO Customer ("aaahgjhgkjhkjhkjhh","Paul","Jean","jp@gmail.com","0151588496","4 rue des suisses","Sucy","gtrsgtgtgsrtgtgstgt",1);
+INSERT INTO Customer ("aaaljkljlkjljghfgd","Paul","Jean","jp@gmail.com","0151588496","7 rue des berges","Sucy","rtgtgrtsrgtgstrtsgtg",0);
+INSERT INTO Customer ("aaagdfhgfjggfgghffh","Paul","Jean","jp@gmail.com","0151588496","99 rue des routes","Sucy","hgfhdjfjfgdfjgfjgfjgfd",1);
+INSERT INTO Customer ("aaahhgfhfhfghfhghhg","Paul","Carl","jp@gmail.com","0151588496","40 rue des mots","Sucy","dsqgsdqgsdqgsqdgsqdgsdqg",1);
+INSERT INTO Customer ("aaahhhhhhhhhhhhhhhhggggggggggffffffffffgggg","Paul","Amelie","jp@gmail.com","0151588496","49 rue des cerises","Sucy","sdgqdgqsgsqdgsdqgsdqg",0);
