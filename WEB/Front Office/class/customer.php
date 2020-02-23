f4186ee258b6dc0730630a63883130a595bcd692
Nicolas <nicolas.fouchard94370@gmail.com>
2020-02-23 18:12:11 +0100

    ADD customer class

---
+++ WEB/Front Office/class/customer.php
@@ -0,0 +1,163 @@
+<?php
+
+class Customer
+{
+    private $id;
+    private $firstname;
+    private $lastname;
+    private $mail;
+    private $phone_number;
+    private $address;
+    private $city;
+    private $password;
+    
+    public function __construct($firstname, $lastname, $mail, $phone_number, $address, $city, $password)
+    {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->mail = $mail;
+        $this->phone_number = $phone_number;
+        $this->address = $address;
+        $this->city = $city;
+        $this->password = $password;
+        $this->id = hash('sha256', $firstname . date('dMY') . $lastname);
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * Get the value of city
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set the value of city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * Get the value of address
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set the value of address
+
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * Get the value of phone_number
+     */
+    public function getPhone_number()
+    {
+        return $this->phone_number;
+    }
+
+    /**
+     * Set the value of phone_number
+
+     */
+    public function setPhone_number($phone_number)
+    {
+        $this->phone_number = $phone_number;
+        return $this;
+    }
+
+    /**
+     * Get the value of mail
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set the value of mail
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+        return $this;
+    }
+
+    /**
+     * Get the value of lastname
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+     * Get the value of firstname
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set the value of firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+}
--- WEB/Front Office/registration.php
+++ WEB/Front Office/registration.php
@@ -2,26 +2,103 @@
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Home Services - Inscription</title>
-    <link rel="icon" sizes="32x32" type="image/png" href="img/favicon.png"/>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-  </head>
-  <body>
 
-    <?php require_once("include/header.php"); ?>
+<head>
+  <meta charset="utf-8">
+  <title>Home Services - Inscription</title>
+  <link rel="icon" sizes="32x32" type="image/png" href="img/favicon.png" />
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
 
-    <main>
+<body>
 
-        <section class="container">
-          <br>
-          <br>
-          <br>
-          <h1>Inscription</h1>
-          <br>
-        <form action="valid_registration.php" method="post">
+  <?php require_once("include/header.php"); ?>
+
+  <main>
+
+    <section class="container">
+      <br>
+      <br>
+      <br>
+      <h1 style="text-align: center;">Inscription</h1>
+
+      <?php
+
+      if (isset($_GET['error'])) {
+
+
+        if ($_GET['error'] == 'captcha_inv') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Erreur, veuillez entrer le bon numéro correspondant à l\'image</li>';
+        }
+
+        if ($_GET['error'] == 'inputs_inv') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Erreur, un ou plusieurs champs n\'ont pas été complétés ou ne sont pas valides</li>';
+        }
+
+        if ($_GET['error'] == 'password_inv') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Veuillez entrer le même mot de passe lors de la confirmation de celui-ci</li>';
+        }
+
+        if ($_GET['error'] == 'password_length') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Veuillez entrer un mot de passe de 6 caractères minimum</li>';
+        }
+
+        if ($_GET['error'] == 'email_inv') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Erreur, veuillez saisir une adresse e-mail valide</li>';
+        }
+
+        if ($_GET['error'] == 'mail_taken') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Cette adresse e-mail est déjà attribuée à un autre compte</li>';
+        }
+
+        if ($_GET['error'] == 'lname_length') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Vous avez dépassé le maximum de caractères possibles pour le champ "Nom"</li>';
+        }
+
+        if ($_GET['error'] == 'fname_length') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Vous avez dépassé le maximum de caractères possibles pour le champ "Prénom"</li>';
+        }
+
+        if ($_GET['error'] == 'city_length') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Vous avez dépassé le maximum de caractères possibles pour le champ "Ville"</li>';
+        }
+
+        if ($_GET['error'] == 'ps_length') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Vous avez dépassé le maximum de caractères possibles pour le champ "Pseudo"</li>';
+        }
+
+        if ($_GET['error'] == 'mail_length') {
+
+          echo '<br>';
+          echo '<li style="color: red;text-align: center;list-style:none;">Vous avez dépassé le maximum de caractères possibles pour le champ "E-mail"</li>';
+        }
+      }
+
+      ?>
+
+      <br>
+      <form action="valid_registration.php" method="POST">
         <div class="form-group">
           <label>Nom</label>
           <input type="text" name="lastname" class="form-control" placeholder="Entrez votre nom" required>
@@ -32,7 +109,7 @@
         </div>
         <div class="form-group">
           <label>Adresse mail</label>
-          <input type="email" class="form-control" placeholder="Enter email" required>
+          <input type="email" name="mail" class="form-control" placeholder="Enter email" required>
           <small id="emailHelp" class="form-text text-muted">Votre adresse mail ne sera pas partagée.</small>
         </div>
         <div class="form-group">
@@ -64,18 +141,19 @@
         </div>
         <button id="regis_button" type="submit" class="btn btn-primary">S'inscrire</button>
       </form>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-        </section>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </section>
 
-    </main>
+  </main>
 
-    <?php require_once("include/footer.php"); ?>
-    <script src="js/password.js"></script>
+  <?php require_once("include/footer.php"); ?>
+  <script src="js/password.js"></script>
 
-  </body>
+</body>
+
 </html>
