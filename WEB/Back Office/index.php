18ec9b130b52f240259d3048a9d2295ae2d0c367
Tony2464 <anthony.fargette@yahoo.fr>
2020-02-24 16:40:05 +0100

    Initial Commit

---
+++ WEB/Back Office/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Home Services - Accueil</title>
+    <link rel="icon" sizes="32x32" type="image/png" href="img/favicon.png" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <?php require_once("include/header.php"); ?>
+
+    <main>
+
+        <section class="container-fluid text-center">
+            <p>Back Office</p>
+        </section>
+
+    </main>
+
+    <?php require_once("include/footer.php"); ?>
+
+</body>
+
+</html>
