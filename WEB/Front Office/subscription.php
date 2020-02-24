192436511e6a47ea0528f5bd8d0568ae1bae447c
Dragboon <ye.steven@hotmail.fr>
2020-02-24 18:03:33 +0100

    CREATE subscription page
    Creation of php and js files

---
+++ WEB/Front Office/subscription.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Page des abonnements</title>
+  <link rel="stylesheet" href="#" type="text/css">
+</head>
+
+<body>
+  <?php require_once('header.php'); ?>
+  <main id="main">
+    <script type="text/javascript" src="js/subscriptionpage.js"></script>
+
+
+  </main>
+  <footer>
+
+  </footer>
+</body>
+
+<?php
+
+
+
+
+?>
+
+
+</html>
