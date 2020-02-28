21924ad373a2294f259a5e89141c7c10626f555d
Dragboon <ye.steven@hotmail.fr>
2020-02-28 14:30:41 +0100

    UPDATE subscription page
    Display of the informations and basic style

--- WEB/Front Office/js/subscriptionpage.js
+++ WEB/Front Office/js/subscriptionpage.js
@@ -5,7 +5,5 @@ window.onload = function() {
 function subscriptionStyle() {
   let subscriptionDiv = document.getElementsByClassName("subscriptions");
   for(let i = 0; i < subscriptionDiv.length; i++) {
-    subscriptionDiv[i].style.border = 'thick solid #000000';
-    //subscriptionDiv[i].style.width = "200px";
   }
 }
--- WEB/Front Office/subscription.php
+++ WEB/Front Office/subscription.php
@@ -11,18 +11,25 @@
 <body>
   <?php require_once __DIR__ . '/include/header.php'; ?>
   <main id="main">
+    <h1> ABONNEMENTS </h1>
     <?php
       $dbmanager = new DBManager($bdd);
       $subscriptions = $dbmanager->getSubscriptionTypes();
-      var_dump($subscriptions);
         foreach ($subscriptions as $subscription) {
           foreach ($subscription as $s) {?>
-            <div class="subscriptions container-fluid">
-              <p> Abonnement <?php echo $s["typeName"]; ?> </p>
-              <p> Service <?php echo $s["openDays"]; ?> </p>
-              <p> Disponible de <?php echo substr($s["openTime"], 0, 2); ?>h à <?php echo substr($s["closeTime"], 0, 2); ?>h </p>
-              <p> <?php echo $s["serviceTimeAmount"] ?>h de service par mois </p>
-              <p> À partir de seulement <?php echo $s["price"] ?>€ !</p>
+            <div class="subscriptions">
+              <h3> Abonnement <?php echo $s["typeName"]; ?> </h3>
+              <p id="subscib_description">
+                <p> <?php if (substr($s["openTime"], 0, 2) != '24') {
+                            echo 'Disponible de ' .
+                            substr($s["openTime"], 0, 2) . 'h à ' .
+                            substr($s["closeTime"], 0, 2) . 'h';
+                          } else echo 'Disponible 24 heures / 24'; ?> </p>
+                <p> <?php echo $s["openDays"]; ?> jours / 7 </p>
+                <p> <?php echo $s["serviceTimeAmount"] ?>h de service par mois </p>
+                <p> À partir de <?php echo $s["price"] ?>€ !</p>
+              </p>
+              <button class="btn btn-secondary subscrib_button">S'abonner !</button>
             </div>
         <?php
           }
