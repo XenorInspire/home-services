89a7fac68b81672ad27137ff94524703b851a9a4
Nicolas <nicolas.fouchard94370@gmail.com>
2020-03-01 23:09:20 +0100

    Merge SQLFusion

---
+++ C/SQLFusion/includes/sqlmanage.h
@@ -0,0 +1,13 @@
+// All these functions are used to read SQL files and write the new one
+
+// Count the lines of SQL files
+int32_t nbLines(const char * fileName);
+
+// This function is charged to read SQL files
+void extractData(DIR_INFO * SQLDirectory, char * fileName);
+
+// Check if the filename has only one extension
+char * verifyExtension(char * fileName);
+
+// Write data in the final SQL file
+void writeSQL(FILE * SQLResult, char ** buffer, int32_t size, char * fileName);
---
+++ C/SQLFusion/includes/struct.h
@@ -0,0 +1,8 @@
+typedef struct DIR_INFO {
+
+    char ** nameSQLFiles;
+    int16_t nbSQLFiles;
+    int16_t nbFiles;
+    DIR * rep;
+
+} DIR_INFO;
