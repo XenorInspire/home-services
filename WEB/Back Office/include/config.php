23f2cd582079968f92486d4f054a15827d8b5462
Tony2464 <anthony.fargette@yahoo.fr>
2020-03-12 21:10:23 +0100

    UPDATE config.php

--- WEB/Back Office/include/config.php
+++ WEB/Back Office/include/config.php
@@ -1,13 +1,19 @@
 <?php
 
+require_once('database.env');
+
+$options = [
+
+	'host=' . DB_HOST,
+	'dbname=' . DB_NAME,
+	'port=' . DB_PORT
+
+];
+
 try {
 
-	$bdd = new PDO('mysql:host=localhost;dbname=home-services','root','root');
-
-}catch(Exception $e) {
+	$bdd = new PDO(DB_DRIVER . ':' . join(';', $options), DB_USER, DB_PASSWORD);
+} catch (Exception $e) {
 
 	die('Erreur : ' . $e->getMessage());
-
 }
-
-?>
