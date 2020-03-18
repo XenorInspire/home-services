28406a5b2472f69e435c0ecbf81751d2c850122d
Nicolas <nicolas.fouchard94370@gmail.com>
2020-03-18 12:42:27 +0100

    HOTFIX mail checking (Ajax)

--- WEB/Front_Office/check_mail.php
+++ WEB/Front_Office/check_mail.php
@@ -12,15 +12,13 @@ if (!isset($_POST['mail']) || empty($_POST['mail'])) {
 $hm_database = new DBManager($bdd);
 $mail = htmlspecialchars($_POST['mail']);
 
-$q = "SELECT customerId FROM customer WHERE email = ?";
+$q = "SELECT customerId FROM Customer WHERE email = ?";
 $req = $hm_database->getDb()->prepare($q);
 $req->execute([$mail]);
 
 $results = [];
 while ($user = $req->fetch())
     $results[] = $user;
-// $results = $req->fetchAll();
-// var_dump($results);
 
 if (count($results) != 0)
     echo "1";
