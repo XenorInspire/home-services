b5f1a18103dee214c68a63a6e7c4fd8d4e3e4caa
Tony2464 <anthony.fargette@yahoo.fr>
2020-04-03 14:50:51 +0200

    UPDATE footer icons

--- WEB/Back_Office/include/footer.php
+++ WEB/Back_Office/include/footer.php
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md2">
-                <a href="index.php?lang=fr" class="btn">Français</a>
+                <a href="index.php?lang=fr" class="btn"><img src="https://img.icons8.com/color/48/000000/france-circular.png" /></a>
             </div>
             <div class="col-md2">
-                <a href="index.php?lang=en" class="btn">Anglais</a>
+                <a href="index.php?lang=en" class="btn"><img src="https://img.icons8.com/color/48/000000/great-britain-circular.png" /></a>
             </div>
         </div>
     </div>
