388cac33c44fcff274aaddb093c216081da134aa
Tony2464 <anthony.fargette@yahoo.fr>
2020-04-12 21:52:17 +0200

    UPDATE add sending email if proposal removed

--- WEB/Back_Office/delete_proposal.php
+++ WEB/Back_Office/delete_proposal.php
@@ -9,6 +9,9 @@ $associateId = $_GET['associateId'];
 $serviceId = $_GET['serviceId'];
 
 $hm_database->deleteAssociateService($serviceId, $associateId);
+$associate = $hm_database->getAssociate($associateId);
+
+system('python3 mail/mail.py cancel_proposal ' . $associate->getEmail() . ' ' . $associate->getAssociateId());
 
 header('Location: reservations.php?delete=successful');
 exit;
