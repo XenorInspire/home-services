b45039655a7f07713b337252513440c01af64637
Tony2464 <anthony.fargette@yahoo.fr>
2020-04-13 14:19:05 +0200

    HOTFIX update subscriptontype

--- WEB/Back_Office/class/DBManager.php
+++ WEB/Back_Office/class/DBManager.php
@@ -97,7 +97,7 @@ class DBManager
         $req->execute([$subName]);
         $data = $req->fetch();
 
-        if ($data != NULL) {
+        if ($data != NULL && $data['typeName'] != $subscription->getTypeName()) {
             header('Location: edit_subscription.php?error=name_taken&id=' . $subscription->getTypeId());
             exit;
         } else {
