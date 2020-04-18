f5fcc9463b996e19f7578e55b9986af3483d558a
Tony2464 <anthony.fargette@yahoo.fr>
2020-04-18 14:18:26 +0200

    ADD generate QRcode for associate

--- WEB/Front_Office/profile_associate.php
+++ WEB/Front_Office/profile_associate.php
@@ -30,6 +30,15 @@
          <br>
          <br>
          <section class="container text-center">
+             <div id="button" class="btn btn-dark" onclick="generateQrcode(); setTimeout(link, 1000);">Regénérer mon QRcode</div>
+             <input id="text" type="hidden" value="<?= $_SESSION['associate'] ?>">
+             <br>
+             <br>
+             <div class="container text-center">
+                 <a id="qrcode" class="text-center"></a>
+             </div>
+             <hr>
+             <br>
              <h1>Mes informations personnelles</h1>
              <br>
              <br>
@@ -37,13 +46,15 @@
                  <div class="col">
                      <div class="input-group-prepend" style="display:inline-block !important;">
                          <span class="input-group-text labels_account">Nom</span>
-                     </div><!--
+                     </div>
+                     <!--
                   --><input type="text" name="lastname" class="form-control inputs_account" value="<?php echo $associate->getLastname(); ?>" disabled>
                  </div>
                  <div class="col">
                      <div class="input-group-prepend" style="display:inline-block !important;">
                          <span class="input-group-text labels_account">Prénom</span>
-                     </div><!--
+                     </div>
+                     <!--
                   --><input type="text" name="firstname" class="form-control inputs_account" value="<?php echo $associate->getFirstname(); ?>" disabled>
                  </div>
              </div>
@@ -52,13 +63,15 @@
                  <div class="col">
                      <div class="input-group-prepend" style="display:inline-block !important;">
                          <span class="input-group-text labels_account">@</span>
-                     </div><!--
+                     </div>
+                     <!--
                   --><input style="width: 79% !important;" name="mail" type="text" class="form-control inputs_account" value="<?php echo $associate->getEmail(); ?>" disabled>
                  </div>
                  <div class="col">
                      <div class="input-group-prepend" style="display:inline-block !important;">
                          <span class="input-group-text labels_account">Téléphone</span>
-                     </div><!--
+                     </div>
+                     <!--
                     --><input style="width: 72% !important;" name="phone_number" type="text" class="form-control inputs_account" value="<?php echo $associate->getPhoneNumber(); ?>" disabled>
                  </div>
              </div>
@@ -88,20 +101,23 @@
                  <div class="col">
                      <div class="input-group-prepend" style="display:inline-block !important;">
                          <span class="input-group-text labels_account">Mot de passe actuel</span>
-                     </div><!--
+                     </div>
+                     <!--
                   --><input style="width: 50% !important;" name="old_password" type="password" class="form-control inputs_account" disabled>
                      <br>
                      <br>
                      <div class="input-group-prepend" style="display:inline-block !important;">
                          <span class="input-group-text labels_account">Nouveau mot de passe</span>
-                     </div><!--
+                     </div>
+                     <!--
                   --><input style="width: 48.1% !important;" id="password_length" onkeyup="checkPassword()" name="new_password" type="password" class="form-control inputs_account" disabled>
                      <br>
                      <small id="password_size" class="form-text">6 caractères minimum</small>
                      <br>
                      <div class="input-group-prepend" style="display:inline-block !important;">
                          <span class="input-group-text labels_account">Confirmation</span>
-                     </div><!--
+                     </div>
+                     <!--
                   --><input id="same" onkeyup="samePassword()" style="width: 54.3% !important;" name="new_password2" type="password" class="form-control inputs_account" disabled>
                      <br>
                      <small id="password_same" class="form-text">Ce mot de passe est différent du champs précédent !</small>
@@ -124,6 +140,37 @@
      <script src="js/password.js"></script>
      <script>
          allocate("<?php echo $id; ?>");
+     </script>
+     <script type="text/javascript" src="js/qrcode.js"></script>
+     <script type="text/javascript">
+         function generateQrcode() {
+             var qrcode = new QRCode(document.getElementById("qrcode"), {
+                 width: 150,
+                 height: 150
+             });
+
+             function makeCode() {
+                 var elText = document.getElementById("text");
+
+                 if (!elText.value) {
+                     alert("Input a text");
+                     elText.focus();
+                     return;
+                 }
+
+                 qrcode.makeCode(elText.value);
+             }
+
+             makeCode();
+             document.getElementById("button").removeAttribute("onclick");
+         }
+
+         function link() {
+             link = document.getElementById("QRcode").src;
+             console.log(link);
+             document.getElementById("qrcode").setAttribute("href", link);
+             document.getElementById("qrcode").setAttribute("download", "QRcode");
+         }
      </script>
  </body>
 
