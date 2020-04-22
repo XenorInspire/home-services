a624bb7585260dac8ba09706eedd0527e61dd0bc
Dragboon <ye.steven@hotmail.fr>
2020-04-22 10:21:06 +0200

    ADD language files

---
+++ WEB/Front_Office/languages/en.php
@@ -0,0 +1,16 @@
+<?php
+$index = array(
+    "description" => "Welcome to Home-Services' Back Office"
+);
+
+$nav = array(
+    "homepage" => "Homepage",
+    "manage" => "Manage",
+    "customers" => "Customer section",
+    "associates" => "Associate section",
+    "subscriptions" => "Subscription section",
+    "reservations" => "Reservations",
+    "services" => "Services",
+    "servicesProvided" => "Services Provided"
+);
+
---
+++ WEB/Front_Office/languages/fr.php
@@ -0,0 +1,91 @@
+<?php
+
+$nav = array(
+
+);
+
+$about_us = array(
+  "aboutUs" => "A propos"
+);
+
+$all_services = array(
+  "serviceList" => "Liste des Services",
+  "chooseCategory" => "Sélectionnez une catégorie :",
+  "category" => "Catégorie",
+  "service" => "Service",
+  "price" => "Prix",
+  "book" => "Réserver"
+);
+
+$associate_proposal_accept = array(
+  "homepage" => "Accueil",
+  "bookOf" => "Réservation de",
+  "serviceInformations" => "Informations du service",
+  "description" => "Description",
+  "date" => "Date",
+  "hour" => "Heure",
+  "place" => "Lieu",
+  "serviceAccept" => "Accepter la prestation",
+  "serviceDecline" => "Refuser la prestation",
+  "serviceAcceptance" => "Acceptation de la prestation",
+  "serviceAcceptanceQuestion" => "Acceptez-vous cette prestation ?",
+  "accept" => "Accepter",
+  "serviceRefusal" => "Refus de la prestation",
+  "serviceRefusalQuestion" => "Refusez-vous cette prestation ?",
+  "decline" => "Refuser",
+  "cancel" => "Annuler"
+);
+
+$associate_services_provided = array(
+  "homepage" => "Accueil",
+  "endConfirmation" => "La prestation a bien été terminée.",
+  "endError" => "Il y a eu une erreur.",
+  "acceptanceConfirmation" => "La prestation a bien été acceptée.",
+  "refusalConfirmation" => "La prestation a bien été refusée.",
+  "serviceRequest" => "Demande de prestation",
+  "reply" => "Répondre",
+  "currentServices" => "Prestations actuelles",
+  "noCurrentServices" => "Aucune prestation en cours",
+  "booking" => "Réservation",
+  "endService" => "Terminer la prestation"
+);
+
+$associate_services = array(
+  "homepage" => "Accueil",
+  "removeServiceConfirmation" => "Le service a bien été retiré.",
+  "addServiceConfirmation" => "Le service a bien été ajouté.",
+  "myServices" => "Mes services",
+  "addToService" => "M'ajouter à un service",
+  "serviceList" => "Liste des services",
+  "remove" => "Retirer",
+  "removeService" => "Retirer un service",
+  "removeConfirmation" => "Êtes-vous certain de vouloir retirer ce service ?",
+  "cancel" => "Annuler",
+  "back" => "Retour"
+);
+
+$book_service = array(
+  "minimum" => "Minimum de ",
+  "price" => "Prix :",
+  "tax" => "TTC",
+  "serviceDate" => "Date de la prestation",
+  "serviceHour" => "Heure de la prestation",
+  "hourAmount" => "Nombre d'heures",
+  "address" => "Adresse",
+  "town" => "Ville",
+  "book" => "Réserver",
+  "cancel" => "Annuler",
+  "booking" => "Réservation",
+  "bookService" => "Voulez-vous réserver ce service ?"
+);
+
+$calendar = array(
+  "services" => "Services",
+  "address" => "Adresse",
+  "hour" => "Heure",
+  "service" => "Service"
+);
+
+$get_reservations = array(
+  "noService" => "Pas de service à cette date."
+);
