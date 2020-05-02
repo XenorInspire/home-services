527c93caf5d771d67de2154b2941a6d8e369b2d3
Dragboon <ye.steven@hotmail.fr>
2020-05-02 18:33:20 +0200

    CLEAN code

--- WEB/Front_Office/js/calendar.js
+++ WEB/Front_Office/js/calendar.js
@@ -6,14 +6,14 @@ let coloredCell;
 class Calendar {
 
   constructor(domTarget) {
-    // On récupère l'élément DOM passé en paramètre
+    // Get calendar div
     domTarget = domTarget || '.calendar';
     this.domElement = document.querySelector(domTarget);
 
-    // Renvoit une erreur si l'élément n'éxiste pas
+    // calendar not found
     if (!this.domElement) throw "Calendar - L'élément spécifié est introuvable";
 
-    // Liste des mois
+    // months list
     if (lang == "fr") {
       this.monthList = new Array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aôut', 'septembre', 'octobre', 'novembre', 'décembre');
     }
@@ -22,7 +22,7 @@ class Calendar {
     }
 
 
-    // Liste des jours de la semaine
+    // days list
     if (lang == "fr") {
       this.dayList = new Array('dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi');
     }
@@ -30,40 +30,40 @@ class Calendar {
       this.dayList = new Array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
     }
 
-    // Date actuelle
+    // today's day number
     this.today = new Date();
     this.today.setHours(0, 0, 0, 0);
 
-    // Mois actuel
+    // today's month
     this.currentMonth = new Date(this.today.getFullYear(), this.today.getMonth(), 1);
 
-    // On créé le div qui contiendra l'entête de notre calendrier
+    // calendar header
     let header = document.createElement('div');
     header.classList.add('header');
     this.domElement.appendChild(header);
 
-    // On créé le div qui contiendra les jours de notre calendrier
+    // calendar days header
     this.content = document.createElement('div');
     this.domElement.appendChild(this.content);
 
-    // Bouton "précédent"
+    // back button
     let previousButton = document.createElement('button');
     previousButton.setAttribute('data-action', '-1');
     previousButton.textContent = '\u003c';
     header.appendChild(previousButton);
 
-    // Div qui contiendra le mois/année affiché
+    // month and year
     this.monthDiv = document.createElement('div');
     this.monthDiv.classList.add('month');
     header.appendChild(this.monthDiv);
 
-    // Bouton "suivant"
+    // next button
     let nextButton = document.createElement('button');
     nextButton.setAttribute('data-action', '1');
     nextButton.textContent = '\u003e';
     header.appendChild(nextButton);
 
-    // Action des boutons "précédent" et "suivant"
+    // buttons back and next functions
     this.domElement.querySelectorAll('button').forEach(element => {
       element.addEventListener('click', () => {
         // On multiplie par 1 les valeurs pour forcer leur convertion en "int"
@@ -72,18 +72,18 @@ class Calendar {
       });
     });
 
-    // On charge le mois actuel
+    // today's month
     this.loadMonth(this.currentMonth);
   }
 
   loadMonth(date) {
-    // On vide notre calendrier
+    // empty div
     this.content.textContent = '';
 
     // On ajoute le mois/année affiché
     this.monthDiv.textContent = this.monthList[date.getMonth()].toUpperCase() + ' ' + date.getFullYear();
 
-    // Création des cellules contenant le jour de la semaine
+    // days of the week cells
     for (let i = 0; i < this.dayList.length; i++) {
       let cell = document.createElement('span');
       cell.classList.add('cell');
@@ -92,7 +92,7 @@ class Calendar {
       this.content.appendChild(cell);
     }
 
-    // Création des cellules vides si nécessaire
+    // empty cells
     for (let i = 0; i < date.getDay(); i++) {
       let cell = document.createElement('span');
       cell.classList.add('cell');
@@ -100,10 +100,10 @@ class Calendar {
       this.content.appendChild(cell);
     }
 
-    // Nombre de jour dans le mois affiché
+    // get number of days of the month
     let monthLength = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
-    // Création des cellules contenant les jours du mois affiché
+    // days of the month cells
     for (let i = 1; i <= monthLength; i++) {
       let cell = document.createElement('p');
       cell.classList.add('cell');
@@ -111,10 +111,10 @@ class Calendar {
       cell.onclick = function () { getServices(this) };
       this.content.appendChild(cell);
 
-      // Timestamp de la cellule
+      // Timestamp
       let timestamp = new Date(date.getFullYear(), date.getMonth(), i).getTime();
 
-      // Ajoute une classe spéciale pour aujourd'hui
+      // special class for today
       if (timestamp === this.today.getTime()) {
         cell.classList.add('today');
         today = cell;
@@ -125,6 +125,7 @@ class Calendar {
 
 const calendar = new Calendar('#calendar');
 
+// get service from a clicked date
 function getServices(d) {
   let space = document.getElementsByClassName('month')[0].innerHTML;
   let month = document.getElementsByClassName('month')[0].innerHTML;
