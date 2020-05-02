d3e7bccb7ca17b23cfb3e62ca6d6731ed43bbf1d
Nicolas <nicolas.fouchard94370@gmail.com>
2020-05-02 12:11:30 +0200

    Compile .jar + clean code

--- JAVA/SQLConnect/src/sqlconnect/Window.java
+++ JAVA/SQLConnect/src/sqlconnect/Window.java
@@ -2,7 +2,6 @@ package sqlconnect;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
